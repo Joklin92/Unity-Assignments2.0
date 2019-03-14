111b4484caeb50d9f3c82edcee43a0b1fd0958e3
Joklin92 <joklin@live.dk>
2019-03-14 10:59:47 +0100

    Project setup

---
+++ Assignment 5/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public static PlayerController instance;
+
+    private int health;
+    public float speed = 10f;
+    
+    void Awake() {
+        if(instance != null) {
+            instance = this;
+        }
+    }
+
+    void Start() {
+    }
+    
+    void Update() {
+
+        if(Input.GetKeyDown("left") || Input.GetKeyDown("a")) {
+            //move left
+            Debug.Log("Moving left");
+        } else if (Input.GetKeyDown("right") || Input.GetKeyDown("d")) {
+            //move right
+            Debug.Log("Moving right");
+        } else if (Input.GetKeyDown("up") || Input.GetKeyDown("w")) {
+            //Jump
+            Debug.Log("Jumping");
+        } else if (Input.GetKeyDown("down") || Input.GetKeyDown("s")) {
+            //crouch
+            Debug.Log("Crouching");
+        }
+    }
+}
