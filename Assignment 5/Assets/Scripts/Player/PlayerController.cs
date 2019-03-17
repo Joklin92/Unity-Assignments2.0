b642204afce8f8119d3c53d8311b051287e0163e
Joklin92 <joklin@live.dk>
2019-03-17 15:00:42 +0100

    Setup related commit

--- Assignment 5/Assets/Scripts/Player/PlayerController.cs
+++ Assignment 5/Assets/Scripts/Player/PlayerController.cs
@@ -1,13 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour {
 
     public static PlayerController instance;
+    public Animator animator;
+
+    public Rigidbody2D playerBody;
 
     private int health;
     public float speed = 10f;
+    public float horizontalMove = 0f; 
     
     void Awake() {
         if(instance != null) {
@@ -16,11 +19,24 @@ public class PlayerController : MonoBehaviour {
     }
 
     void Start() {
+        playerBody = GetComponent<Rigidbody2D>();
     }
     
     void Update() {
 
-        if(Input.GetKeyDown("left") || Input.GetKeyDown("a")) {
+        //   if (canMove) {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+
+          //  playerBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed;
+        playerBody.velocity = new Vector2(horizontalMove, Input.GetAxisRaw("Vertical") * speed);
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove)); 
+/*  
+        } else {
+            playerBody.velocity = Vector2.zero;
+        }*/
+
+
+        if (Input.GetKeyDown("left") || Input.GetKeyDown("a")) {
             //move left
             Debug.Log("Moving left");
         } else if (Input.GetKeyDown("right") || Input.GetKeyDown("d")) {
@@ -33,5 +49,25 @@ public class PlayerController : MonoBehaviour {
             //crouch
             Debug.Log("Crouching");
         }
+
+        if(Input.GetKeyDown(KeyCode.K)) {
+            Die();
+        }
+
     }
+
+    private void OnCollisionEnter(Collision col) {
+        if(col.gameObject.name == "Enemy") {
+            Die();
+        }
+    }
+
+    void Die() {
+        Debug.Log("Die");
+
+        animator.SetBool("Dead", true);
+        this.enabled = false;
+
+    }
+
 }
